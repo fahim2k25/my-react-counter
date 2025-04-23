6b914c4fa2e144baa1fea14b7548c294fd7990a9
fahim2k25 <fahim.program@gmail.com>
2025-04-23 22:49:51 +0600

    Added the counter component with click

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
+
 export default function Counter() {
 
+    // using useState hook from REACT
+    const [count, setCount] = useState(0);
+
+    // adding event handler for the add button
+    const handleAdd = () => {
+        let newCount = count + 1;
+        setCount(newCount);
+    }
+
+    // Creating style object
     const counterStyle = {
 
         border: "2px solid yellow",
@@ -9,8 +21,8 @@ export default function Counter() {
     return (
         <div style={counterStyle}>
             <h2>Counter</h2>
-            <h3>Count: </h3>
-            <button>Add</button>
+            <h3>Count: {count}</h3>
+            <button onClick={handleAdd} >Add</button>
         </div>
 
     )
